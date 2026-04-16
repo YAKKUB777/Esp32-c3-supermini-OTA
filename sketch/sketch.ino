61fb17668e390a894f2cd025ee51ebb956b97043
YAKKUB777 <m.yakkub00@gmail.com>
2026-04-16 22:47:22 +0300

    Update sketch.ino

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,31 +1,40 @@
 #include <SPI.h>
 #include <RF24.h>
+#include <nRF24L01.h>
 
+// Піни для ESP32-C3 SuperMini
 #define CE_PIN   8
 #define CSN_PIN  7
 
 RF24 radio(CE_PIN, CSN_PIN);
 
 void setup() {
+  // Налаштування SPI для Ci24R1 (3-wire mode)
+  // Нагадування: з'єднайте MOSI (5) та MISO (6) через резистор 1к до DATA модуля
   radio.begin();
-  radio.setAddressWidth(3); 
-  radio.setPALevel(RF24_PA_MAX);      [span_6](start_span)// Максимум 11dBm[span_6](end_span)
-  radio.setDataRate(RF24_2MBPS);     // Максимальна ширина спектра одного пакета
-  radio.setAutoAck(false);           // Вимикаємо перевірку, щоб не чекати відповіді
+  
+  [span_0](start_span)[span_1](start_span)// Максимальна потужність 11dBm згідно з мануалом[span_0](end_span)[span_1](end_span)
+  radio.setPALevel(RF24_PA_MAX); 
+  radio.setDataRate(RF24_2MBPS);
+  radio.setAutoAck(false);
+  radio.setRetries(0, 0);
   radio.stopListening();
+
+  // Прямий запис у регістр для активації постійного випромінювання (CONT_WAVE)
+  uint8_t setup_reg = radio.read_register(RF_SETUP);
+  radio.write_register(RF_SETUP, setup_reg | 0x80); 
 }
 
 void loop() {
-  // Швидко проходимо по основних каналах Wi-Fi (1, 6, 11) 
-  [span_7](start_span)// або по всьому діапазону 0-125[span_7](end_span)
+  [span_2](start_span)// Цикл швидкого "прошивання" частот від 2.400 до 2.525 ГГц[span_2](end_span)
   for (int i = 0; i < 126; i++) {
     radio.setChannel(i);
     
-    // Відправляємо "сміттєвий" пакет без очікування
-    const uint8_t dummy[] = {0xAA, 0xAA, 0xAA, 0xAA};
-    radio.startWrite(&dummy, sizeof(dummy), true); 
+    // Відправка порожнього пакету для модуляції шуму
+    const uint8_t jam_data[] = {0xFF, 0xFF, 0xFF, 0xFF};
+    radio.startWrite(&jam_data, sizeof(jam_data), true);
     
-    // Мінімальна затримка для стабілізації синтезатора частоти
-    delayMicroseconds(10); 
+    // Дуже коротка затримка для охоплення всього спектру
+    delayMicroseconds(50); 
   }
 }
