0be0a69a8ed5507a100c07e874415d9463e48f16
YAKKUB777 <m.yakkub00@gmail.com>
2026-04-29 17:07:07 +0300

    Update config.h

--- sketch/config.h
+++ sketch/config.h
@@ -1,24 +1,32 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-// ===== ДИСПЛЕЙ ST7735 =====
-#define TFT_CS    5
-#define TFT_DC    6
-#define TFT_RST   7
-#define TFT_MOSI  10
-#define TFT_SCK   8
-#define TFT_LED   4   // підключати до 3.3V напряму, не через GPIO
+// ===== Піни дисплея ST7735 0.96" RGB 160x80 =====
+#define TFT_CS     5
+#define TFT_DC     6
+#define TFT_RST    7
+#define TFT_SCLK   8
+#define TFT_MOSI   10
 
-// ===== PN532 I2C =====
-#define PN532_SDA  20
-#define PN532_SCL  21
+// ===== Піни NRF24L01 =====
+#define NRF_CE     2
+#define NRF_CSN    3
+#define NRF_MISO   21
 
-// ===== КНОПКИ =====
-#define BTN_SCAN     0   // GPIO0, INPUT_PULLUP, замикає на GND
-#define BTN_EMULATE  1   // GPIO1, INPUT_PULLUP, замикає на GND
+// ===== Кольори =====
+#define C_BG        ST77XX_BLACK
+#define C_HEAD      ST77XX_CYAN
+#define C_OK        ST77XX_GREEN
+#define C_ERR       ST77XX_RED
+#define C_WARN      ST77XX_YELLOW
+#define C_TEXT      ST77XX_WHITE
+#define C_GRAPH     ST77XX_GREEN
+#define C_PEAK      ST77XX_RED
+#define C_GRID      0x4208
 
-// ===== ПАРАМЕТРИ =====
-#define MAX_UID_LEN   7   // максимальна довжина UID (Mifare = 4, 7 байт)
-#define EMULATE_TIME  5000 // час емуляції в мс
+// ===== Налаштування NRF24 =====
+#define SCAN_MIN_CH     0
+#define SCAN_MAX_CH     125
+#define SCAN_DELAY_US   128
 
-#endif // CONFIG_H
+#endif
